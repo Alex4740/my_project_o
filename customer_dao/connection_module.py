51d3fbc87772be7f21611a6db9d76754778757fe
Alemayehu145 <100173700+Alemayehu145@users.noreply.github.com>
2022-03-16 15:58:22 -0400

    connection and customer dao

---
+++ customer_dao/connection_module.py
@@ -0,0 +1,22 @@
+import os
+
+from psycopg import connect
+
+
+def create_connection():
+    try:
+        conn = connect(
+
+            host=os.environ.get("HOST"),
+            dbname=os.environ.get("DBNAME"),
+            user=os.environ.get("USER"),
+            password=os.environ.get("PASSWORD"),
+            port=os.environ.get("PORT")
+        )
+        return conn
+    except ConnectionError as e:
+        print(str(e))
+
+
+connection = create_connection()
+print(connection)
---
+++ customer_dao/customer_dao_imp.py
@@ -0,0 +1,26 @@
+from customer_dao.connection_module import connection
+from customer_dao.customer_dao_interface import CustomerDAOInterface
+from entities.customer_class import Customer
+from entities.nothing_deleted import NothingDeleted
+
+
+class CustomerDAOImp(CustomerDAOInterface):
+
+    def create_customer_record(self, customer: Customer) -> Customer:
+        sql = "insert into customers values(default, %s, %s) returning customer_id"
+        cursor = connection.cursor
+        cursor.execute(sql, (customer.first_name, customer.last_name))
+        connection.commit()
+        returned_id = cursor.fetchone()[0]
+        customer.customer_id = returned_id
+        return customer
+
+    def delete_customer_record(self, customer_id: int) -> bool:
+        sql = "delete from customers where customer_id = %s"
+        cursor = connection.Cursor()
+        cursor.execute(sql, [customer_id])
+        connection.commit()
+        if cursor.rowcount != 0:
+            return True
+        else:
+            raise NothingDeleted("there was no customer with the given ID")
